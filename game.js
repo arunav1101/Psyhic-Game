773a65a26c88db6d6766640f930dac5a3ed5e599
Arunav Gupta <arunav1101@gmail.com>
2019-02-03 13:37:54 -0800

    fix as per review

---
+++ game.js
@@ -0,0 +1,68 @@
+
+function computerChoice() {
+  let computerChoices = "abcdefghiklmnopqrstuvwxyz";
+  let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  return computerGuess;
+}
+// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+// var computerChoices = [a-z];
+
+var wins, losses, chances, computerGuess, userGuess, myoptions = [];
+
+function setup() {
+  wins = 0;
+  losses = 0;
+}
+
+function varSetup() {
+  myoptions = [];
+  userGuess = '';
+  computerGuess = '';
+  chances = 9;
+}
+
+//Initialise the Game
+setup();
+varSetup();
+
+// Create variables that hold references to the places in the HTML where we want to display things.
+var directionsText = document.getElementById("directions-text");
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var chanceText = document.getElementById("chance-text");
+var myoptionsText = document.getElementById("myoptions-text");
+// This function is run whenever the user presses a key.
+document.onkeyup = function (event) {
+  // regex to match wrong key event
+  if (event.key.match(/[!@#$%^&*(),.?":{}|<> 0-9]/g)) {
+    alert('Enter From a-z');
+  } else {
+    computerGuess = computerChoice();
+    console.log('Computer Chose==>', computerGuess);
+    // Determines which key was pressed.
+    userGuess = event.key;
+    console.log('User Chose==>', userGuess);
+    userGuess = userGuess.toLowerCase();
+    myoptions.push(userGuess);
+    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+    if (userGuess === computerGuess) {
+      wins++;
+      varSetup();
+      alert('Awesome! You Got Me!')
+    } else {
+      chances -= 1;
+    }
+    if (chances <= 0) {
+      losses++;
+      varSetup();
+    }
+    // Logging on the screen
+    directionsText.textContent = "Guess what letter I am thinking of!";
+    winsText.textContent = "wins: " + wins;
+    lossesText.textContent = "losses: " + losses;
+    chanceText.textContent = "Guess Left: " + chances;
+    myoptionsText.textContent = ` Your Guesses So far:  ${myoptions}`;
+  }
+}
