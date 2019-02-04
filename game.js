76b7072e3f9248877d9d74605c25260af962994e
Arunav Gupta <arunav1101@gmail.com>
2019-02-03 23:03:20 -0800

    fix issue

--- game.js
+++ game.js
@@ -1,8 +1,7 @@
-
 function computerChoice() {
-  let computerChoices = "abcdefghiklmnopqrstuvwxyz";
-  let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  return computerGuess;
+    let computerChoices = "abcdefghiklmnopqrstuvwxyz";
+    let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    return computerGuess;
 }
 // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
 // var computerChoices = [a-z];
@@ -10,15 +9,15 @@ function computerChoice() {
 var wins, losses, chances, computerGuess, userGuess, myoptions = [];
 
 function setup() {
-  wins = 0;
-  losses = 0;
+    wins = 0;
+    losses = 0;
 }
 
 function varSetup() {
-  myoptions = [];
-  userGuess = '';
-  computerGuess = '';
-  chances = 9;
+    myoptions = [];
+    userGuess = '';
+    //   computerGuess = '';
+    chances = 9;
 }
 
 //Initialise the Game
@@ -34,35 +33,37 @@ var lossesText = document.getElementById("losses-text");
 var chanceText = document.getElementById("chance-text");
 var myoptionsText = document.getElementById("myoptions-text");
 // This function is run whenever the user presses a key.
+computerGuess = computerChoice();
 document.onkeyup = function (event) {
-  // regex to match wrong key event
-  if (event.key.match(/[!@#$%^&*(),.?":{}|<> 0-9]/g)) {
-    alert('Enter From a-z');
-  } else {
-    computerGuess = computerChoice();
-    console.log('Computer Chose==>', computerGuess);
-    // Determines which key was pressed.
-    userGuess = event.key;
-    console.log('User Chose==>', userGuess);
-    userGuess = userGuess.toLowerCase();
-    myoptions.push(userGuess);
-    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-    if (userGuess === computerGuess) {
-      wins++;
-      varSetup();
-      alert('Awesome! You Got Me!')
+    // regex to match wrong key event
+    if (event.key.match(/[!@#$%^&*(),.?":{}|<> 0-9]/g)) {
+        alert('Enter From a-z');
     } else {
-      chances -= 1;
+        console.log('Computer Chose==>', computerGuess);
+        // Determines which key was pressed.
+        userGuess = event.key;
+        console.log('User Chose==>', userGuess);
+        userGuess = userGuess.toLowerCase();
+        myoptions.push(userGuess);
+        // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+        if (userGuess === computerGuess) {
+            wins++;
+            varSetup();
+            computerGuess = computerChoice();
+            alert('Awesome! You Got Me!')
+        } else {
+            chances -= 1;
+        }
+        if (chances <= 0) {
+            losses++;
+            computerGuess = computerChoice();
+            varSetup();
+        }
+        // Logging on the screen
+        directionsText.textContent = "Guess what letter I am thinking of!";
+        winsText.textContent = "wins: " + wins;
+        lossesText.textContent = "losses: " + losses;
+        chanceText.textContent = "Guess Left: " + chances;
+        myoptionsText.textContent = ` Your Guesses So far:  ${myoptions}`;
     }
-    if (chances <= 0) {
-      losses++;
-      varSetup();
-    }
-    // Logging on the screen
-    directionsText.textContent = "Guess what letter I am thinking of!";
-    winsText.textContent = "wins: " + wins;
-    lossesText.textContent = "losses: " + losses;
-    chanceText.textContent = "Guess Left: " + chances;
-    myoptionsText.textContent = ` Your Guesses So far:  ${myoptions}`;
-  }
 }
